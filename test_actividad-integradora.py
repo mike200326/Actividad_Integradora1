20c40ac5bac425f7d0e2c91c7afaf8d115fbe7ed
Angel Orlando <A01659000@tec.mx>
2024-09-07 17:11:18 -0600

    Modificacion de transmission1 y transmission2, al igual que las pruebas

--- test_actividad-integradora.py
+++ test_actividad-integradora.py
@@ -11,7 +11,7 @@ mcode2 = read_file('mcode2_content.txt')
 mcode3 = read_file('mcode3_content.txt')
 
 def test_contains_code():
-    assert contains_code(transmission1, mcode1) == 'true 23'
+    assert contains_code(transmission1, mcode1) == 'true 27'
     assert contains_code(transmission1, mcode2) == 'false'
     assert contains_code(transmission1, mcode3) == 'false'
     assert contains_code(transmission2, mcode1) == 'false'
@@ -19,12 +19,11 @@ def test_contains_code():
     assert contains_code(transmission2, mcode3) == 'false'
 
 def test_find_longest_palindrom():
-    assert find_longest_palindrome(transmission1) == '1 1'
-    assert find_longest_palindrome(transmission2) == '1 1'
+    assert find_longest_palindrome(transmission1) == '11 14' # Palìndromo: 0AA0
+    assert find_longest_palindrome(transmission2) == '10 12' # Palindromo: E6E
 
 def test_longest_common_substring():
-    assert longest_common_substring(transmission1, transmission2) == '17 18'
+    assert longest_common_substring(transmission1, transmission2) == '21 22'
 
 if __name__ == "__main__":
-    
     pytest.main()
